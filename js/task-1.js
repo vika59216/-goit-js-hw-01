7971e55f494597b5155cbe50651cb7d54b5e03d9
vika59216 <viktoriiadupreez@gmail.com>
2023-11-15 09:52:37 +0100

    hw1

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+
+
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    const totalPrice = quantity * pricePerDroid;
+    }
+   
+
+
+    makeTransaction(5, 3000);
+    makeTransaction(3, 1000);
+    makeTransaction(10, 500);
---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+
+
+function getShippingMessage(country, price, deliveryFee){
+   return `Shipping to ${country} will cost ${totalPrice} credits`
+    const totalPrice = price + deliveryFee;
+}
+
+
+
+console.log(getShippingMessage("Australia", 120, 50)); 
+console.log(getShippingMessage("Germany", 80, 20));
+ console.log(getShippingMessage("Sweden", 100, 20));
